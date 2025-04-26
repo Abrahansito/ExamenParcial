bd479220822254d459b4be411752976c8a0330b6
Abrahansito <huamancarlosabraham>
2025-04-25 23:03:14 -0500

    Modificaciones

--- Controllers/AssignmentController.cs
+++ Controllers/AssignmentController.cs
@@ -5,6 +5,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;   
+using ExamenParcial.Models;
+using ExamenParcial.Data; 
+
 
 namespace ExamenParcial.Controllers
 {
@@ -12,6 +17,7 @@ namespace ExamenParcial.Controllers
     public class AssignmentController : Controller
     {
         private readonly ILogger<AssignmentController> _logger;
+        
 
         public AssignmentController(ILogger<AssignmentController> logger)
         {
@@ -23,59 +29,7 @@ namespace ExamenParcial.Controllers
             return View();
         }
 
-        public async Task<IActionResult> Asociar()
-        {
-        ViewBag.Players = new SelectList(_context.Players, "Id", "Nombre");
-        ViewBag.Teams = new SelectList(_context.Teams, "Id", "Nombre");
-        return View();
-        }
-
-        [HttpPost]
-        public async Task<IActionResult> Asociar(int PlayerId, int TeamId)
-        {
-        var existe = await _context.Assignments.AnyAsync(a => a.PlayerId == PlayerId && a.TeamId == TeamId);
-        if (!existe)
-
-        {
-        _context.Add(new Assignment { PlayerId = PlayerId, TeamId = TeamId });
-        await _context.SaveChangesAsync();
-
-        }
-
-        return RedirectToAction("Listar");
-
-        public async Task<IActionResult> Listar()
-        {
-        var data = await _context.Assignments
-                .Include(a => a.Player)
-                .Include(a => a.Team)
-                .ToListAsync();
-                
-        return View(data);
-        }
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
